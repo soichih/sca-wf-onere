1ee453ad1b40f109c14a580f95a55138cdf0531a
Ben Serrette <bserrett@iu.edu>
2016-08-09 13:39:02 -0400

    Dev dependencies installed.

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp'),
+	util = require("gulp-util"),
+	sass = require('gulp-sass'),
+	livereload = require('gulp-livereload'),
+	log = util.log;
+
+var sassFiles = "*.scss";
+
+gulp.task('default', function(){
+
+});
+
+gulp.task('sass', function(){
+	log("Generate CSS files " + (new Date()).toString());
+	gulp.src(sassFiles)
+		.pipe(sass({ style: 'expanded' }))
+		.pipe(gulp.dest(""))
+		;
+});
+
+gulp.task('reload', function(){
+	gulp.src("*")
+		.pipe(livereload());
+});
+
+gulp.task("watch", function(){
+	livereload.listen();
+
+	//log("Watching scss files for modifications.");
+	gulp.watch(sassFiles, ["sass"]);
+	gulp.watch(["*.html", "**/*.html", "**/*.php", "**/*.ejs", "**/*.css", "**/*.js"], ["reload"]);
+	console.log("Watching...");
+});
